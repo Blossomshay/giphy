fc147106b03490e9eabd85f5862874a8e1481b4b
Blossomshay <sbute@tywls.org>
2019-04-02 22:47:54 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -4,8 +4,21 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+var choice= $('#search-term').val();
+
+$.ajax({
+    url: "https://api.giphy.com/v1/gifs/search?q="+choice+"&rating=pg&api_key=dc6zaTOxFJmzC",
+    method: "GET",
+    success:function(response) {
+        var randIndex=Math.floor(Math.random()*response.data.length);
+        $('.gallery').html (
+             '<div>' +
+             '<img src=' + response.data[randIndex].images.fixed_width.url + '/>'
+             +'</div>');
+        },
+
+
+});
+
 });
 
